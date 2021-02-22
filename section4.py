d580b627d15256823c45bc0e1a397976809cea22
TheCricri <73289652+TheCricri@users.noreply.github.com>
2021-02-22 21:29:59 +0100

    Add files via upload

---
+++ section4.py
@@ -0,0 +1,32 @@
+"""
+University of Liege
+INFO8003-1 - Optimal decision making for complex problems
+Assignment 2 - Reinforcement Learning in a Continuous Domain
+By:
+    PIRLET Matthias
+    CHRISTIAENS Nicolas
+"""
+
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import ExtraTreesClassifier
+from keras.models import Sequential
+from keras.layers import Dense
+
+def Fitted_Q_iteration(set_tuples,model):
+    N = 0
+    Q_N = 0
+    
+    return Q_N
+
+def Liner_Regression():
+    return
+
+def Extremely_Randomized_Trees():
+    return
+
+def Neural_networks():
+    return
+
+if __name__ == "__main__":
+   print("0")
