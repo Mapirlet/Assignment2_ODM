000aa599e04d3c8dd104bf080958909c1668bf81
TheCricri <73289652+TheCricri@users.noreply.github.com>
2021-02-28 18:51:26 +0100

    Add files via upload

--- section5.py
+++ section5.py
@@ -36,29 +36,40 @@ def Neural_Networks(i,o):
     model.fit(i,o)
     return model
 
-def derived_policy(domain,Q_map,resolution):
+def derived_policy(domain,Q_fct,resolution,plot=False):
     
     p_vector = np.linspace(-1,1,int(2/resolution)+1)
     l_p = len(p_vector)
     s_vector = np.linspace(-3,3,int(6/resolution)+1)
     l_s = len(s_vector)
     actions = domain.actions
+    l_a = len(actions)
     X, Y = np.meshgrid(p_vector,s_vector)
     policy = np.zeros([l_s,l_p])
-
+    
+    Q_map = np.zeros([l_s,l_p,l_a])
+    for i in range(l_p):
+        for j in range(l_s):
+            for k in range(l_a):
+                p = p_vector[i]
+                s = s_vector[j]
+                u = actions[k]
+                Q_map[j,i,k] = Q_fct.predict(np.array([p,s,u]).reshape(-1,3))
+    
     for p in range(l_p):
         for s in range(l_s):
             index = np.argmax(Q_map[s,p])
             policy[s,p] = actions[index]
-            
-    min_v = -4
-    max_v = 4
-    fig3, ax3 = plt.subplots()
-    c = ax3.contourf(X,Y,policy,cmap='RdBu',vmax=max_v,vmin=min_v)
-    fig3.colorbar(c)
-    ax3.set_title('Policy (blue = 4) / (red = -4)')
-    ax3.set_xlabel('Position')
-    ax3.set_ylabel('Speed')
+      
+    if plot == True:
+        min_v = -4
+        max_v = 4
+        fig1, ax1 = plt.subplots()
+        c = ax1.contourf(X,Y,policy,cmap='RdBu',vmax=max_v,vmin=min_v)
+        fig1.colorbar(c)
+        ax1.set_title('Policy (blue = 4) / (red = -4)')
+        ax1.set_xlabel('Position')
+        ax1.set_ylabel('Speed')
 
     policy_object = MyPolicy(policy,resolution)
     expected = compute_expected_return(domain,500,policy_object.getPolicy)
